fdba5a82250b90722d4e22926e75ad53d6ed8d59
AndrewNation II <129698289+andrewnationdev@users.noreply.github.com>
2023-11-09 14:31:36 -0300

    página nova

---
+++ js/actions.js
@@ -0,0 +1,6 @@
+function navigate(target) {
+    if(target === "home")
+        window.navigate("https://andrewnationdev.github.io/biversetheory/");
+
+    window.navigate(`https://andrewnationdev.github.io/biversetheory/${target}.html`);
+}
